b8572c9dcd851a8d1351d11ed2d8f43f546de4ca
weihuang <thoughtworks.ht.huang@mbusa.com>
2025-07-15 14:32:17 +0800

    integration test

---
+++ src/test/java/com/bootcamp/springBootDemo/EmployeeControllerTest.java
@@ -0,0 +1,60 @@
+package com.bootcamp.springBootDemo;
+
+import com.bootcamp.springBootDemo.model.Employee;
+import com.bootcamp.springBootDemo.model.Gender;
+import com.bootcamp.springBootDemo.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeControllerTest {
+
+    @Autowired
+    private MockMvc client;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @BeforeEach
+    public void setup() {
+        employeeRepository.getAll().clear();
+        employeeRepository.save(new Employee(1, "John Smith", 32, Gender.MALE, 5000.0));
+        employeeRepository.save(new Employee(2, "Jane Johnson", 28, Gender.FEMALE, 6000.0));
+        employeeRepository.save(new Employee(3, "David Williams", 35, Gender.MALE, 5500.0));
+        employeeRepository.save(new Employee(4, "Emily Brown", 23, Gender.FEMALE, 4500.0));
+        employeeRepository.save(new Employee(5, "Michael Jones", 40, Gender.MALE, 7000.0));
+    }
+
+    @Test
+    public void should_return_employees_when_get_all_employees_exist() throws Exception {
+        // given
+        List<Employee> givenEmployees = employeeRepository.getAll();
+
+        // when
+        ResultActions perform = client.perform(MockMvcRequestBuilders.get("/employees"));
+
+        // then
+        perform.andExpect(MockMvcResultMatchers.status().isOk());
+        perform.andExpect(MockMvcResultMatchers.jsonPath("$.[0].id").value(givenEmployees.get(0).getId()));
+        perform.andExpect(MockMvcResultMatchers.jsonPath("$.[0].name").value(givenEmployees.get(0).getName()));
+        perform.andExpect(MockMvcResultMatchers.jsonPath("$.[0].age").value(givenEmployees.get(0).getAge()));
+        perform.andExpect(MockMvcResultMatchers.jsonPath("$.[0].gender").value(givenEmployees.get(0).getGender().name()));
+        perform.andExpect(MockMvcResultMatchers.jsonPath("$.[0].salary").value(givenEmployees.get(0).getSalary()));
+        perform.andExpect(MockMvcResultMatchers.jsonPath("$.[1].id").value(givenEmployees.get(1).getId()));
+        perform.andExpect(MockMvcResultMatchers.jsonPath("$.[2].id").value(givenEmployees.get(2).getId()));
+        perform.andExpect(MockMvcResultMatchers.jsonPath("$.[3].id").value(givenEmployees.get(3).getId()));
+        perform.andExpect(MockMvcResultMatchers.jsonPath("$.[4].id").value(givenEmployees.get(4).getId()));
+    }
+}
+
+// please implement the rest of employee apis
